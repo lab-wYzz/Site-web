483900817f2cc7809f89f3afdc73c1b0788f0e48
KEIL_Enzo <enzo.keil06@icloud.com>
2023-03-08 17:13:32 +0100

    page inscription en cours

--- Site-Ydays-lab/pages/Inscription/inscription.php
+++ Site-Ydays-lab/pages/Inscription/inscription.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
-    <title>Bienvenue sur wyzz - Connectez vous pour commencer</title>
+    <title>Bienvenue sur wyzz - Inscrivez vous pour commencer l'aventure</title>
     <link rel="stylesheet" href="../../index.css">
 </head>
 
@@ -26,7 +26,23 @@
         <div class="container">
 
             <div class="form">
-                <h2>Connexion</h2>
+                <h2>Inscription</h2>
+                <p class="input-container">
+                    <input type="email" id="input-password" class="login-input">
+                    <label for="input-password" unselectable="on">Email</label>
+                </p>
+
+                <div class="items" id="filiere">
+                    <label class="items">Filiere : </label>
+                    <select id="selectfil" class="items" name="filiere">
+                        <option value="informatique">Informatique</option>
+                        <option value="audiovisuel">Audio Visuel</option>
+                        <option value="webmanagement">Technology & Business</option>
+                        <option value="marketcomm">Marketing & Communication</option>
+                        <option value="archi">Architecture d'intérieur</option>
+                        <option value="3danim">3D, Animation & Jeux-vidéo</option>
+                    </select>
+                </div>
                 <p class="input-container">
                     <input type="text" id="input-username" class="login-input">
                     <label for="input-username" unselectable="on">Username</label>
@@ -35,8 +51,11 @@
                     <input type="password" id="input-password" class="login-input">
                     <label for="input-password" unselectable="on">Password</label>
                 </p>
-                <p class="inscription">Toujours pas inscrits ? <a
-                        href="../../pages/Inscription/inscription.php">Inscrivez-vous</a>
+                <p class="input-container">
+                    <input type="password" id="input-password" class="login-input">
+                    <label for="input-password" unselectable="on">Confirmer votre Password</label>
+                </p>
+                <p class="inscription">Deja inscrits ? <a href="../../pages/Connexion/connexion.php">Connectez-vous</a>
                 </p>
                 <div class="validation">
                     <input type="submit" class="btn" value="Valider">
