638fa4a52d1563c567c92c2481c15b9a7186ac15
KEIL_Enzo <enzo.keil06@icloud.com>
2023-03-08 17:29:41 +0100

    reglage erreur console (css)

--- Site-Ydays-lab/compenents/footer.php
+++ Site-Ydays-lab/compenents/footer.php
@@ -14,9 +14,7 @@
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-
     <title>Footer</title>
-    <link rel="stylesheet" href="../index.css">
 </head>
 
 <body>
