a7282acafe73a5993180dd1b697992aaba0d480e
KEIL_Enzo <enzo.keil06@icloud.com>
2023-03-08 16:40:24 +0100

    base de donee relier avec page d'accueil

--- Site-Ydays-lab/pages/Accueil/accueil.php
+++ Site-Ydays-lab/pages/Accueil/accueil.php
@@ -1,16 +1,26 @@
-<!-- 
-        session_start();
+<?php
 
-        // empêcher l'utilisateur d'accédé à la page s'il n'est pas connecté en le renvoyant sur la page de connexion
-        if ($_SESSION['id_user'] == "" || $_SESSION['email'] == "" || $_SESSION['pseudo'] == "" || $_SESSION['filiere'] == "" || $_SESSION['pass_user'] == "" || $_SESSION['xp'] == "") {
-            header('Location: ../login/logout.php');
-            exit();
-        }
+$user = "root";
+$pass = "";
+$bdd = new PDO('mysql:host=localhost;dbname=ywzz', $user, $pass);
 
-        $xp = $_SESSION['xp'];
-        $progbar = round((($xp * 100) / 10000), 0)
+try {
 
-        -->
+    $stmt = $bdd->prepare("SELECT pseudo, xp, filiere FROM user ");
+
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+
+    usort($result, function ($a, $b) {
+        $a = $a['xp'];
+        $b = $b['xp'];
+        return ($a == $b) ? 0 : (($a < $b) ? 1 : -1);
+    });
+    $ranking = 1;
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -22,13 +32,9 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;400;700&family=Mina:wght@400;700&family=Zen+Dots&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;400;700&family=Mina:wght@400;700&family=Zen+Dots&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
@@ -77,28 +83,14 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td data-label="pseudo">ibra</td>
-                    <td data-label="xp">15008</td>
-                    <td data-label="rang">1</td>
-                </tr>
-                <tr>
-                    <td scope="row" data-label="pseudo">ibra</td>
-                    <td data-label="xp">15008</td>
-                    <td data-label="rang">2</td>
-
-                </tr>
-                <tr>
-                    <td scope="row" data-label="pseudo">ibra</td>
-                    <td data-label="xp">15008</td>
-                    <td data-label="rang">3</td>
-                </tr>
-                <tr>
-                    <td scope="row" data-label="pseudo">ibra</td>
-                    <td data-label="xp">15008</td>
-                    <td data-label="rang">4</td>
-
-                </tr>
+                <?php foreach ($result as $row) : ?>
+                    <tr>
+                        <td data-label="pseudo"><?php echo $row['pseudo'] ?></td>
+                        <td data-label="xp"><?php echo $row['xp'] ?></td>
+                        <td data-label="rang"><?php echo "$ranking" ?></td>
+                    </tr>
+                    <?php $ranking++; ?>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </main>
@@ -108,7 +100,7 @@
 
     <script src="../../JS/accueil.js"></script>
     <script type="module">
-    import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+        import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
     </script>
 </body>
 
