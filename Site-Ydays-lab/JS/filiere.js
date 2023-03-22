0a6e68ee5c7351a4e51f87915211af6edef80621
KEIL_Enzo <enzo.keil06@icloud.com>
2023-03-22 12:18:17 +0100

    page question filiere

--- Site-Ydays-lab/JS/filiere.js
+++ Site-Ydays-lab/JS/filiere.js
@@ -1,17 +1,16 @@
+
+
 //Fenetre modal
 const modalBtn = document.getElementById("modal-btn");
 const modal = document.getElementById("modal");
 const overlay = document.querySelector(".overlay");
 
-if (localStorage.getItem("popupClosedJustePrix")) {
-    modal.style.display = "none";
-    overlay.style.display = "none";
-}
 
 const closePopup = () => {
     modal.style.opacity = "0";
     modal.style.transform = "translateX(1200px)";
     overlay.style.opacity = "0";
+    overlay.style.transform = "translateX(1200px)";
     // localStorage.setItem("popupClosedJustePrix", true);
 };
 
