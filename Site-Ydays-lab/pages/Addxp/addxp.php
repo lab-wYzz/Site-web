1ae417f35ef13ed30cb29e37d3cd9605256c2474
Sqwado <mateo.luque@ynov.com>
2023-04-05 09:26:57 +0200

    serie xp in quest

--- Site-Ydays-lab/pages/Addxp/addxp.php
+++ Site-Ydays-lab/pages/Addxp/addxp.php
@@ -1,10 +1,24 @@
-
 <?php
 
 session_start();
+$id_user = $_SESSION["id_user"];
+$xp = $_SESSION["xp_to_add"];
 
-echo($_SESSION["xp_to_add"].", ");
-echo($_SESSION["quest_stage"]);
+$dbh = new PDO('mysql:host=localhost;dbname=wyzz', "root", "");
+$requete = "UPDATE user SET xp = xp + '$xp' WHERE id_user = '$id_user'";
+$result = $dbh->exec($requete);
 
+echo ($_SESSION["xp_to_add"] . ", ");
+$serie_xp = "";
+
+foreach ($_SESSION["serie_xp"] as $elm) {
+    $serie_xp = $serie_xp . ", " . $elm;
+}
+
+$_SESSION["quest_stage"] = 1;
+$_SESSION["xp_added"] = $_SESSION["xp_to_add"];
+$_SESSION["xp_to_add"] = 0;
+
+echo ($serie_xp. " , ".$_SESSION["xp_added"]);
 
 ?>
