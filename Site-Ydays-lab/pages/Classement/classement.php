8d59fbdf24dfd57c26f6489597b8f3f1677b2d4b
Sqwado <mateo.luque@ynov.com>
2023-04-19 09:39:53 +0200

    session start classement

--- Site-Ydays-lab/pages/Classement/classement.php
+++ Site-Ydays-lab/pages/Classement/classement.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 // empêcher l'utilisateur d'accédé à la page s'il n'est pas connecté en le renvoyant sur la page de connexion
 if ($_SESSION['id_user'] == "" || $_SESSION['email'] == "" || $_SESSION['pseudo'] == "" || $_SESSION['filiere'] == "" || $_SESSION['pass_user'] == "" || $_SESSION['xp'] == "") {   
     header('Location: ../login/logout.php');
