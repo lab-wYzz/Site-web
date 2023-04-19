ea966df833e8734e9920315c81d497b208395905
KEIL_Enzo <enzo.keil06@icloud.com>
2023-04-19 14:41:35 +0200

    style memories

--- Site-Ydays-lab/pages/jeux/reflexe.php
+++ Site-Ydays-lab/pages/jeux/reflexe.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Jeu de réflexe</title>
 	<link rel="stylesheet" type="text/css" href="reflexe.css">
+	<link rel="stylesheet" href="../../index.css">
 </head>
+
 <body>
 	<div id="game">
 		<div class="row">
-		  <div class="color-box" id="red"></div>
-		  <div class="color-box" id="green"></div>
+			<div class="color-box" id="red"></div>
+			<div class="color-box" id="green"></div>
 		</div>
 		<div class="row">
-		  <div class="color-box" id="blue"></div>
-		  <div class="color-box" id="yellow"></div>
+			<div class="color-box" id="blue"></div>
+			<div class="color-box" id="yellow"></div>
 		</div>
-		<button id="start">Commencer</button>
-	  </div>
+		<input id="modal-btn" type="button" value="Jouer" class="btn">
 
-	<script src="reflexe.js"></script>
+	</div>
+
+	<script src="../../JS/memories.js"></script>
 </body>
+
 </html>
